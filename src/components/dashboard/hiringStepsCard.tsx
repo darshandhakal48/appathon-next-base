525ecac60b5aec989d81668627e018815a84d944
Prakash Gurung <prakashgrg4400@gmail.com>
2025-06-19 22:23:18 +0545

    completed the basic layout for the admin dashboard page

--- src/components/dashboard/hiringStepsCard.tsx
+++ src/components/dashboard/hiringStepsCard.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { FileCheck } from "lucide-react";
 
-function Card() {
+function HiringStepsCard() {
     return (
         <div className="bg-background flex justify-center items-center gap-6 p-6 rounded-[6px] md:px-9">
-            <div className="bg-background-secondary rounded-full w-[52px] h-[52px]  border-2 flex justify-center items-center">
-                <FileCheck className="h-[24px] w-[24px]  " />
+            <div className="bg-muted rounded-full w-[52px] h-[52px]  flex justify-center items-center">
+                <FileCheck className="h-[24px] w-[24px] text-primary-surface    " />
             </div>
             <div className="flex flex-col">
                 <span className="text-sm">Applied</span>
@@ -15,4 +15,4 @@ function Card() {
     );
 }
 
-export default Card;
+export default HiringStepsCard;
